c1de9a79f2486291efa380f7507704e090ec57c7
nguyenhuyen <64192153+havy862@users.noreply.github.com>
2020-04-23 21:04:21 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Chuong3P1Bai6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DanhSach httta = new DanhSach();
+            httta.nhap();
+            httta.xuat();
+            httta.SapXep();
+            httta.XuatDS();
+            httta.LocDiem();
+            Console.ReadLine();
+        }
+    }
+}
